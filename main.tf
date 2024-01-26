5e74910f218030124f0a09573f3f2ffbfed5cf2f
Shaik Latheef <143796689+latheef1403@users.noreply.github.com>
2024-01-26 11:07:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,41 +2,39 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
 
-resource "aws_subnet" "sub1" {
+resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "sub2" {
+resource "aws_subnet" "subnet2" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "igw1" {
   vpc_id = aws_vpc.myvpc.id
 }
 
-resource "aws_route_table" "RT" {
+resource "aws_route_table" "route1" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw1.id
   }
 }
 
 resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
-  route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route1.id
 }
 
 resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.sub2.id
-  route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route1.id
 }
 
 resource "aws_security_group" "webSg" {
